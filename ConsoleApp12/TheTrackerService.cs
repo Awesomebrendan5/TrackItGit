3022395cf4e3e000c7cf00146a530d9a6229a988
Awesomebrendan <116621279+Awesomebrendan5@users.noreply.github.com>
2023-07-11 20:28:48 +1000

    Furthermore, I added a system to fix bugs in my screentime monitoring system, firstly, my application now has a variable named NonNullName, this is saved whenever a loop is run where the "textlength" property of the "ApplicationNamed" variable is greater than 0. This enables my application to react when a user closes an application and the window's handle is no longer valid. The application reacts through noticing that the handle for the current, now closed window is null through the textlength property being 0 characters for the invalid handle, and therefore, it runs a method which creates a record utilising the NonNullName that was saved prior. Finally, I discovered that my program can only handle application names up to a certain length, and therefore, added compatability for applications that save a large path such as Google Chrome and Microsoft Word through simplifying their names. I intend to add compatability in this way for much of the Microsoft Office and Adobe suites

--- ConsoleApp12/TheTrackerService.cs
+++ ConsoleApp12/TheTrackerService.cs
@@ -24,6 +24,7 @@ namespace TheTracker
         [DllImport("user32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
         static extern int GetWindowTextLength(IntPtr hWnd);
         private readonly System.Timers.Timer t;
+        public string NonNullName;
 
         public class ScreentimeStats
         {
@@ -40,6 +41,7 @@ namespace TheTracker
             Int32 OpenApplication = CurrentWindow.ToInt32(); //Saves that window as an integer.
             IntPtr OldOpenWindow = CurrentWindow; //Saves the currently open window as OldOpenWindow.
             Stopwatch ScreenTimer = new Stopwatch();
+            Startup();
             ScreenTimer.Start();
             String path = "C:\\Users\\brend\\source\\repos\\TrackIt\\TrackIt\\Storage6.csv";
             if (File.Exists(path))
@@ -51,10 +53,10 @@ namespace TheTracker
                 Fileexists = false;
             }
 
-            t = new System.Timers.Timer(10000) { AutoReset = true };
+            System.Timers.Timer t = new System.Timers.Timer(10000) { AutoReset = true };
+            t.Start();
             t.Elapsed += OnEventExecution;
             Console.ReadLine();
-            Startup();
             void Startup()
             {
                 String path = AppDomain.CurrentDomain.BaseDirectory;
@@ -63,49 +65,186 @@ namespace TheTracker
             }
             void OnEventExecution(object sender, System.Timers.ElapsedEventArgs e)
             {
-                IntPtr NewOpenWindow = GetForegroundWindow(); //Defines NewOpenWindow as the window now open.
-                if (OldOpenWindow == NewOpenWindow) //Checks if the OldOpenWindow is the same as the NewOpenWindow.
+                Console.WriteLine("Ran" + DateTime.Now);
+                try
                 {
+
+                    {
+                        IntPtr NewOpenWindow = GetForegroundWindow(); //Defines NewOpenWindow as the window now open.
+                        if (OldOpenWindow == NewOpenWindow) //Checks if the OldOpenWindow is the same as the NewOpenWindow.
+                        {
+                            int textLength = GetWindowTextLength(CurrentWindow);
+                            StringBuilder ApplicationName = new StringBuilder(textLength + 1);
+                            GetWindowText(CurrentWindow, ApplicationName, ApplicationName.Capacity);
+                            String ApplicationNamed = ApplicationName.ToString();
+                            NonNullName = ApplicationNamed;
+                        }
+                        if (OldOpenWindow != NewOpenWindow) //Checks if the OldOpenWindow is different to the NewOpenWindow. 
+                        {
+                            int textLength = GetWindowTextLength(CurrentWindow);
+                            StringBuilder ApplicationName = new StringBuilder(textLength + 1);
+                            GetWindowText(CurrentWindow, ApplicationName, ApplicationName.Capacity);
+                            DateTime DateCollected = DateTime.Now;
+                            long TimerDuration = ScreenTimer.ElapsedMilliseconds;
+                            String ApplicationNamed = ApplicationName.ToString();
+                            if (textLength > 0)
+                            {
+                                if (ApplicationNamed.Contains("Google Chrome"))
+                                {
+                                    ApplicationNamed = "Google Chrome";
+                                    Console.WriteLine("works");
+                                }
+                                NonNullName = ApplicationNamed;
+                                if (ApplicationNamed.Contains("Notepad++"))
+                                {
+                                    ApplicationNamed = "Notepad++";
+                                    Console.WriteLine("works");
+                                }
+                                if (ApplicationNamed.Contains("TrackIt"))
+                                {
+                                    ApplicationNamed = "TrackIt";
+                                    Console.WriteLine("works");
+                                }
+                                if (ApplicationNamed.Contains("Edge"))
+                                {
+                                    ApplicationNamed = "Microsoft Edge";
+                                    Console.WriteLine("works");
+                                }
+                                if (ApplicationNamed.Contains("Firefox"))
+                                {
+                                    ApplicationNamed = "Firefox";
+                                    Console.WriteLine("works");
+                                }
+                                if (ApplicationNamed.Contains("Discord"))
+                                {
+                                    ApplicationNamed = "Discord";
+                                    Console.WriteLine("works");
+                                }
+                                if (ApplicationNamed.Contains("Opera"))
+                                {
+                                    ApplicationNamed = "Opera";
+                                    Console.WriteLine("works");
+                                }
+                                if (ApplicationNamed.Contains("OneNote"))
+                                {
+                                    ApplicationNamed = "OneNote";
+                                    Console.WriteLine("works");
+                                }
+                                if (ApplicationNamed.Contains("Word"))
+                                {
+                                    ApplicationNamed = "Microsoft Word";
+                                    Console.WriteLine("works");
+                                }
+                                if (ApplicationNamed.Contains("PowerPoint"))
+                                {
+                                    ApplicationNamed = "Powerpoint";
+                                    Console.WriteLine("works");
+                                }
+                                if (ApplicationNamed.Contains("Excel"))
+                                {
+                                    ApplicationNamed = "Excel";
+                                    Console.WriteLine("works");
+                                }
+                                if (ApplicationNamed.Contains("Access"))
+                                {
+                                    ApplicationNamed = "Access";
+                                    Console.WriteLine("works");
+                                }
+                                if (ApplicationNamed.Contains("Outlook"))
+                                {
+                                    ApplicationNamed = "Outlook";
+                                    Console.WriteLine("works");
+                                }
+                                if (ApplicationNamed.Contains("Adobe XD"))
+                                {
+                                    ApplicationNamed = "Adobe XD";
+                                    Console.WriteLine("works");
+                                }
+                                if (ApplicationNamed.Contains("Adobe Acrobat"))
+                                {
+                                    ApplicationNamed = "Adobe Acrobat";
+                                    Console.WriteLine("works");
+                                }
+                                if (ApplicationNamed.Contains("Microsoft Teams"))
+                                {
+                                    ApplicationNamed = "Microsoft Teams";
+                                    Console.WriteLine("works");
+                                }
+                                if (!String.IsNullOrEmpty(ApplicationNamed))
+                                {
+                                    var records = new List<ScreentimeStats>
+                                    {
+                                    new ScreentimeStats { ApplicationName = ApplicationNamed, ScreenTimeCollect = TimerDuration, DateCollected = DateCollected}
+                                    };
+                                    if (Fileexists == false)
+                                    {
+                                        using (var writer = new StreamWriter("C:\\Users\\brend\\source\\repos\\TrackIt\\TrackIt\\Storage6.csv"))
+                                        using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+                                        {
+                                            csv.WriteRecords(records);
+                                        }
+                                        Fileexists = true;
+                                    }
+                                    if (Fileexists == true)
+                                    {
+                                        // Append to the file.
+                                        var config = new CsvConfiguration(CultureInfo.InvariantCulture)
+                                        {
+                                            // Don't write the header again.
+                                            HasHeaderRecord = false,
+                                        };
+                                        using (var stream = File.Open("C:\\Users\\brend\\source\\repos\\TrackIt\\TrackIt\\Storage6.csv", FileMode.Append))
+                                        using (var writer = new StreamWriter(stream))
+                                        using (var csv = new CsvWriter(writer, config))
+                                        {
+                                            csv.WriteRecords(records);
+                                        }
+                                    }
+                                    ScreenTimer.Restart();
+                                    CurrentWindow = NewOpenWindow;
+                                    OldOpenWindow = NewOpenWindow;
+                                }
+                            }
+                            if (textLength <= 0)
+                            {
+                                var records = new List<ScreentimeStats>
+                                    {
+                                    new ScreentimeStats { ApplicationName = NonNullName, ScreenTimeCollect = TimerDuration, DateCollected = DateCollected}
+                                    };
+                                if (Fileexists == false)
+                                {
+                                    using (var writer = new StreamWriter("C:\\Users\\brend\\source\\repos\\TrackIt\\TrackIt\\Storage6.csv"))
+                                    using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+                                    {
+                                        csv.WriteRecords(records);
+                                    }
+                                    Fileexists = true;
+                                }
+                                if (Fileexists == true)
+                                {
+                                    // Append to the file.
+                                    var config = new CsvConfiguration(CultureInfo.InvariantCulture)
+                                    {
+                                        // Don't write the header again.
+                                        HasHeaderRecord = false,
+                                    };
+                                    using (var stream = File.Open("C:\\Users\\brend\\source\\repos\\TrackIt\\TrackIt\\Storage6.csv", FileMode.Append))
+                                    using (var writer = new StreamWriter(stream))
+                                    using (var csv = new CsvWriter(writer, config))
+                                    {
+                                        csv.WriteRecords(records);
+                                    }
+                                }
+                                ScreenTimer.Restart();
+                                CurrentWindow = NewOpenWindow;
+                                OldOpenWindow = NewOpenWindow;
+                            }
+                        }
+                    }
                 }
-                if (OldOpenWindow != NewOpenWindow) //Checks if the OldOpenWindow is different to the NewOpenWindow. 
+                catch
                 {
-                    int textLength = GetWindowTextLength(CurrentWindow);
-                    StringBuilder ApplicationName = new StringBuilder(textLength + 1);
-                    GetWindowText(CurrentWindow, ApplicationName, ApplicationName.Capacity);
-                    DateTime DateCollected = DateTime.Now;
-                    long TimerDuration = ScreenTimer.ElapsedMilliseconds;
-                    String ApplicationNamed = ApplicationName.ToString();
-                    var records = new List<ScreentimeStats>
-                        {
-                            new ScreentimeStats { ApplicationName = ApplicationNamed, ScreenTimeCollect = TimerDuration, DateCollected = DateCollected}
-                        };
-                    if (Fileexists == false)
-                    {
-                        using (var writer = new StreamWriter("C:\\Users\\brend\\source\\repos\\TrackIt\\TrackIt\\Storage6.csv"))
-                        using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
-                        {
-                            csv.WriteRecords(records);
-                        }
-                        Fileexists = true;
-                    }
-                    if (Fileexists == true)
-                    {
-                        // Append to the file.
-                        var config = new CsvConfiguration(CultureInfo.InvariantCulture)
-                        {
-                            // Don't write the header again.
-                            HasHeaderRecord = false,
-                        };
-                        using (var stream = File.Open("C:\\Users\\brend\\source\\repos\\TrackIt\\TrackIt\\Storage6.csv", FileMode.Append))
-                        using (var writer = new StreamWriter(stream))
-                        using (var csv = new CsvWriter(writer, config))
-                        {
-                            csv.WriteRecords(records);
-                        }
-                    }
-                    ScreenTimer.Restart();
-                    CurrentWindow = NewOpenWindow;
-                    OldOpenWindow = NewOpenWindow;
+                    Console.WriteLine("An exception occurred");
                 }
             }
         }
